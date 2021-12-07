d12691c8bad5bda0a555e7b5fee3604902a7fd1b
ayala bouhnik <abouhnik8@gmail.com>
2021-12-07 18:32:11 +0200

    initial commit

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.helloworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnLogin=findViewById<Button>(R.id.btnLogin)//גישה לכפתור "היכנס"
+        val edUsername=findViewById<EditText>(R.id.edUsername)
+        val edPassword=findViewById<EditText>(R.id.edPassword)
+        val tvRegister=findViewById<TextView>(R.id.tvRegister)
+
+        btnLogin.setOnClickListener {//כל מה שנכתוב כאן יקרה ברגע שנלחץ על בכפתור
+            if(edUsername.text.trim().isNotEmpty() || edPassword.text.trim().isNotEmpty()){
+                Toast.makeText(this, "input provided", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "input required", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        tvRegister.setOnClickListener {
+            val intent=Intent(this,activityRegister::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ HelloWorld/app/src/main/java/com/example/helloworld/activityHomeP.kt
@@ -0,0 +1,11 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activityHomeP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_p)
+    }
+}
